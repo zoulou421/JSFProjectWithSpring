cffebcd6a98d6dbe8968bfb37f0634de4e61b0d1
Bonevy BEBY <bonevybeby@gmail.com>
2023-08-17 16:55:57 +0200

    initial commit- Integrating Spring, JSF, and Eclipse through Maven & web.xml

---
+++ PlantsPlacesJSFTest/src/main/java/com/formationkilo/ui/MonApplication.java
@@ -0,0 +1,24 @@
+package com.formationkilo.ui;
+
+import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+@Named
+@ManagedBean
+@Scope("session")
+public class MonApplication {
+
+	private String slogan="Apprendre apprendre du JSF avec  formationkilo";
+
+	public String getSlogan() {
+		return slogan;
+	}
+
+	public void setSlogan(String slogan) {
+		this.slogan = slogan;
+	}
+	
+	
+}
