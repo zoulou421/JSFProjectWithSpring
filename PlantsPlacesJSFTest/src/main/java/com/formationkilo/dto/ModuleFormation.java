555dca28f93b2ba18be8a837033c2e17ad6501e5
Bonevy BEBY <bonevybeby@gmail.com>
2023-08-17 19:33:54 +0200

    +package com.formationkilo.dto, Bean: ModuleFormation

---
+++ PlantsPlacesJSFTest/src/main/java/com/formationkilo/dto/ModuleFormation.java
@@ -0,0 +1,22 @@
+package com.formationkilo.dto;
+
+import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+@Named
+@ManagedBean
+@Scope("session")
+public class ModuleFormation {
+
+  private String name;
+
+  public String getName() {
+	return name;
+  }
+  public void setName(String name) {
+	this.name = name;
+  }
+  
+}
