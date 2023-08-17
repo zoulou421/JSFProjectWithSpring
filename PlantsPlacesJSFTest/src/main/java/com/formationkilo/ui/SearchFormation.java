4e46a7c54804d3f4b3937898565f439fce772a84
Bonevy BEBY <bonevybeby@gmail.com>
2023-08-17 22:27:35 +0200

    Accessing Spring Session ManagedBean from another SMB in JSF

--- PlantsPlacesJSFTest/src/main/java/com/formationkilo/ui/SearchFormation.java
+++ PlantsPlacesJSFTest/src/main/java/com/formationkilo/ui/SearchFormation.java
@@ -1,16 +1,34 @@
 package com.formationkilo.ui;
 
 import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.springframework.context.annotation.Scope;
+
+import com.formationkilo.dto.ModuleFormation;
 
 @Named
 @ManagedBean
 @Scope("session")
 public class SearchFormation {
 
+	@Inject
+	private ModuleFormation moduleFormation;
+	
+	public ModuleFormation getModuleFormation() {
+		return moduleFormation;
+	}
+
+	public void setModuleFormation(ModuleFormation moduleFormation) {
+		this.moduleFormation = moduleFormation;
+	}
+
 	public String execute() {
-		return "noresults";
+		if(moduleFormation!=null && moduleFormation.getName().equalsIgnoreCase("JavaEE")) {
+			return "search";
+		}else {
+			return "noresults";
+		}
 	}
 }
