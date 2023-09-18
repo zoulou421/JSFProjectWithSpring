88dce2108f34a7a50a8233ceff29ad7b8ae46fe0
Bonevy BEBY <bonevybeby@gmail.com>
2023-09-18 23:07:18 +0200

    Create and populate Specimen DTO

--- PlantsPlacesJSFTest/src/main/java/com/formationkilo/ui/LieuVO.java
+++ PlantsPlacesJSFTest/src/main/java/com/formationkilo/ui/LieuVO.java
@@ -1,10 +1,12 @@
 package com.formationkilo.ui;
 
 import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.springframework.context.annotation.Scope;
 
+import com.formationkilo.dto.Lieu;
 import com.formationkilo.dto.ModuleFormation;
 
 @Named
@@ -14,11 +16,27 @@ public class LieuVO {
 
 	ModuleFormation moduleFormation;
 
+	@Inject
+	private Lieu lieu;
+	
+	public Lieu getLieu() {
+		return lieu;
+	}
+
+	public void setLieu(Lieu lieu) {
+		this.lieu = lieu;
+	}
+
 	public ModuleFormation getModuleFormation() {
 		return moduleFormation;
 	}
 
 	public void setModuleFormation(ModuleFormation moduleFormation) {
 		this.moduleFormation = moduleFormation;
+	}
+	
+	public String save() {
+		int i=1+1;
+		return "lieu saved";
 	}
 }
